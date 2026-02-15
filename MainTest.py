6812387e9680e2518624cdf667a0d3c103a33b70
Santiago <saraujo24a@udenar.edu.co>
2026-02-14 22:57:38 -0500

    Cambio menu

--- MainTest.py
+++ MainTest.py
@@ -6,6 +6,8 @@ num2 = int(input())
 print("Elige una operacion: ")
 print("1. Suma")
 print("2. Resta")
+print("3. Multiplicacion")
+print("4. Division")
 
 opcion = int(input())
 match opcion:
@@ -13,5 +15,9 @@ match opcion:
         sumarNumeros(num1, num2)
     case 2:
         restarNumeros(num1, num2)
+    case 3:
+        multiplicarNumeros(num1, num2)
+    case 4:
+        dividirNumeros(num1, num2)
     case other:
         print("Opcion no valida")
