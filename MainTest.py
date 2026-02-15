f1795051b4226f04fc775e0711ad319718d4295d
Aston-GH <miangel2754@gmail.com>
2026-02-15 00:10:31 -0500

    Perra pero no hecha por harold

--- MainTest.py
+++ MainTest.py
@@ -1,12 +1,9 @@
 from multiplicacion import multiplicarNumeros
 from division import dividirNumeros
-
-<<<<<<< HEAD
+from resta import restarNumeros
 
 from suma import sumarNumeros
 
-=======
->>>>>>> main
 print("Propiedad de OtroServer.github")
 print("Ingrese Numero 1: ")
 num1 = int(input())
--- resta.py
+++ resta.py
@@ -1,3 +1,3 @@
-def restaNumeros(a, b):
+def restarNumeros(a, b):
     resultado = a - b
     print("El resultado de la resta de esta monda es:", resultado)
