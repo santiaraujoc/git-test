2843001d9db62e896f8b40f7dd396e8f0d80b284
Harold-pm <hapanchalo24A@udeanar.edu.co>
2026-02-15 00:42:37 -0500

    contribucion

--- resta.py
+++ resta.py
@@ -1,2 +1,3 @@
 def restarNumeros(num1, num2):
-    print(f"El resultado de la resta es: {num1 - num2}")
+    resultado = num1 - num2
+    print("El resultado de la resta desta monda es: ", resultado)
