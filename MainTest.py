f300de6b4bed86ea3deccd79e12f90c079449bd1
Santiago <saraujo24a@udenar.edu.co>
2026-02-14 22:18:22 -0500

    Primer commit

---
+++ MainTest.py
@@ -0,0 +1,17 @@
+print("Ingrese Numero 1: ")
+num1 = int(input())
+print("Ingrese Numero 2: ") 
+num2 = int(input())
+
+print("Elige una operacion: ")
+print("1. Suma")
+print("2. Resta")
+
+opcion = int(input())
+match opcion:
+    case 1:
+        sumarNumeros(num1, num2)
+    case 2:
+        restarNumeros(num1, num2)
+    case other:
+        print("Opcion no valida")
